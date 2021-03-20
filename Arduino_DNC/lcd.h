465436d79810a7919c26aced9de5cba5c4dbdceb
= <naveed.ajaj@gmail.com>
2021-03-20 19:54:54 +0530

    Arduino DNC Project

---
+++ Arduino_DNC/lcd.h
@@ -0,0 +1,8 @@
+#include "stdafx.h"
+LCDWIKI_KBV lcd(ILI9486,A3,A2,A1,A0,A4); //model,cs,cd,wr,rd,reset
+
+class tft
+{
+	
+}
+
---
+++ Arduino_DNC/stdafx.h
@@ -0,0 +1,3 @@
+#include <TouchScreen.h> //touch library
+#include <LCDWIKI_GUI.h> //Core graphics library
+#include <LCDWIKI_KBV.h> //Hardware-specific library
